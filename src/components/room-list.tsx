4aac6f01499b31f563d60a9f86710748dd393608
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-08-23 19:33:04 -0300

    feat(components): add question form and question-item

--- src/components/room-list.tsx
+++ src/components/room-list.tsx
@@ -22,7 +22,7 @@ export function RoomList() {
         {isLoading && <p className="text-muted-foreground text-sm">Carregando salas...</p>}
         {data?.map((room) => {
           return (
-            <Link key={room.id} className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent/50" to={`/rooms/${room.id}`}>
+            <Link key={room.id} className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent/50" to={`/room/${room.id}`}>
               <div className="flex-1 flex flex-col gap-1">
                 <h3 className="font-medium ">{room.name}</h3>
                 <div className="flex items-center gap-2">
