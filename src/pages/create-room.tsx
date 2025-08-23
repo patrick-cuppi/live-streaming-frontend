2a3e451ef8f72b025e7d66d7b13d724056a5ad87
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-08-23 16:45:59 -0300

    feat(page): form to create a new room

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,3 +1,4 @@
+import { CreateRoomForm } from "@/components/create-room-form";
 import { RoomList } from "@/components/room-list";
 
 export function CreateRoom() {
@@ -5,8 +6,7 @@ export function CreateRoom() {
     <div className="min-h-screen py-8 px-4">
       <div className="mx-auto max-w-4xl">
         <div className="grid gap-8 grid-cols-2 items-start">
-          <div>Formulário Aqui</div>
-
+          <CreateRoomForm />
           <RoomList />
         </div>
       </div>
