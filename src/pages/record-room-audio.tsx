00fa0ff20ad0181d251b5716929394eff5b432b0
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-08-26 12:40:58 -0300

    feat(audio): send an audio in multiple chunks

--- src/pages/record-room-audio.tsx
+++ src/pages/record-room-audio.tsx
@@ -14,16 +14,17 @@ export function RecordRoomAudio() {
   const params = useParams<RoomParams>()
   const [isRecording, setIsRecording] = useState(false)
   const recorder = useRef<MediaRecorder | null>(null)
-
-  if (!params.roomId) {
-    return <Navigate replace to="/" />
-  }
+  const intervalRef = useRef<NodeJS.Timeout>(null)
 
   function stopRecording() {
     setIsRecording(false)
 
     if (recorder.current && recorder.current.state !== 'inactive') {
       recorder.current.stop()
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current)
     }
   }
 
@@ -42,6 +43,30 @@ export function RecordRoomAudio() {
     console.log(result)
   }
 
+  function createRecorder(audio: MediaStream) {
+    recorder.current = new MediaRecorder(audio, {
+        mimeType: 'audio/webm',
+        audioBitsPerSecond: 64_000,
+    })
+
+    recorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        
+        uploadAudio(event.data)
+      }
+    }
+
+    recorder.current.onstart = () => {
+      console.log('Gravação iniciada')
+    }
+
+    recorder.current.onstop = () => {
+      console.log('Gravação finalizada')
+    }
+
+    recorder.current.start()
+  }
+
   async function startRecording() {
     if (!isRecordingSupported) {
       alert('Seu navegador não suporta gravação de áudio')
@@ -50,40 +75,26 @@ export function RecordRoomAudio() {
 
     setIsRecording(true)
 
-    try {
-      const audio = await navigator.mediaDevices.getUserMedia({
-        audio: {
-          echoCancellation: true,
-          noiseSuppression: true,
-          sampleRate: 44_100,
-        }, 
-      })
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      }, 
+    })
 
-      recorder.current = new MediaRecorder(audio, {
-        mimeType: 'audio/webm',
-        audioBitsPerSecond: 64_000,
-      })
+    createRecorder(audio)
 
-      recorder.current.ondataavailable = (event) => {
-        if (event.data.size > 0) {
-          
-          uploadAudio(event.data)
-        }
-      }
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop()
 
-      recorder.current.onstart = () => {
-        console.log('Gravação iniciada')
-      }
+      createRecorder(audio)
+    }, 5000)
 
-      recorder.current.onstop = () => {
-        console.log('Gravação finalizada')
-      }
-
-      recorder.current.start()
-
-    } catch (error) {
-      console.error(error)
+    if (!params.roomId) {
+      return <Navigate replace to="/" />
     }
+
   }
  
   return (
